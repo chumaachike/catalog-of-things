4c327510a56522f4b3392f8e3938b476aa5490b3
worashf <uppert83@gmail.com>
2022-09-06 23:48:14 +0300

    Add item class

---
+++ classes/item.rb
@@ -0,0 +1,41 @@
+require 'date'
+
+class Item
+  attr_accessor :published_date
+  attr_reader :id, :archived, :label, :source, :author, :genre
+
+  def initialize(published_date, archived: false)
+    @id = Random.rand(1..1000)
+    @published_date = Date.parse(published_date)
+    @archived = archived
+  end
+
+  def source=(source)
+    return if source.nil? # return if source is nil
+
+    @source = source
+
+    source.add_item(self) unless source.items.includes?(self) # add source if the source is not in the items
+  end
+
+  def genre=(genre)
+    return if genre.nil? # return if genre is nil
+
+    @genre = genre
+    genre.add_item(self) unless genre.items.includes?(self) # add genre if gener is not in teh items
+  end
+
+  def author=(author)
+    return if author.nil? # return if author is nil
+
+    @author = author
+    author.add_item(self) unless author.items.includes?(self) # add author  if the author is not in the items
+  end
+
+  def label=(label)
+    return if label.nil? # return if label is nil
+
+    @label = label
+    label.add_item(self) unless label.items.includes?(self) # add label to items array  if it is not in the items
+  end
+end
