3e9d775722a5d987996cf151cc1124a2b8a23653
worashf <uppert83@gmail.com>
2022-09-07 21:55:33 +0300

    Fix linters and update book test

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,19 +1,37 @@
 require './classes/book'
+require 'date'
 
 describe Book do
   context 'Initizlize book' do
     book1 = Book.new('o realy', 'Good', '12-06-2020', archived: false, id: 501)
     book2 = Book.new('o realy', 'Bad', '12-06-2020', archived: true, id: 502)
 
-    it 'Can_be_archivedd? should return  false' do
-      expect(book1.can_be_archived?).to be false
-      expect(book2.can_be_archived?).to be true
-    end
     it 'should be return book id, cover_state, publisher and published date' do
       expect(book1.id).to eq 501
       expect(book2.id).to eq 502
       expect(book1.publisher).to eq 'o realy'
       expect(book1).to be_an_instance_of Book
+    end
+
+    it 'Book should be class' do
+      expect(Book).to be_a Class
+    end
+  end
+  context 'can_be_archived? spec' do
+    published_date = Time.now.year - 10
+    book3 = Book.new('o realy', 'Bad1', published_date, archived: true, id: 503)
+    it 'should retun false published date less than 10  and cover state not Bad ' do
+      expect(book3.can_be_archived?).to be false
+    end
+    published_date = Time.now.year - 15
+    book4 = Book.new('o realy', 'Bad1', published_date, archived: true, id: 504)
+    it 'should retun true published date greater than 10  and cover state not Bad ' do
+      expect(book4.can_be_archived?).to be true
+    end
+    published_date = Time.now.year - 15
+    book5 = Book.new('o realy', 'Bad', published_date, archived: true, id: 505)
+    it 'should retun true published date greater than 10  and cover state is Bad ' do
+      expect(book5.can_be_archived?).to be true
     end
   end
 end
