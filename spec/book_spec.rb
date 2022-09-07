96be98b0aab68a1de5fa5a160e6d4a69d00ef9f0
worashf <uppert83@gmail.com>
2022-09-07 20:00:38 +0300

    Add more spec

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,13 +1,19 @@
 require './classes/book'
 
+describe Book do
+  context 'Initizlize book' do
+    book1 = Book.new('o realy', 'Good', '12-06-2020', archived: false, id: 501)
+    book2 = Book.new('o realy', 'Bad', '12-06-2020', archived: true, id: 502)
 
-describe Book  do 
-    context "Initizlize book" do 
-        book = Book.new('o realy', 'Good', '12-06-2020', archived: false, id: 501)
-
-        it 'Should return publisher, cover state, published date, archived state and id' do
-          expect(book.can_be_archived?).to be false
-        end
+    it 'Can_be_archivedd? should return  false' do
+      expect(book1.can_be_archived?).to be false
+      expect(book2.can_be_archived?).to be true
     end
-
+    it 'should be return book id, cover_state, publisher and published date' do
+      expect(book1.id).to eq 501
+      expect(book2.id).to eq 502
+      expect(book1.publisher).to eq 'o realy'
+      expect(book1).to be_an_instance_of Book
+    end
+  end
 end
