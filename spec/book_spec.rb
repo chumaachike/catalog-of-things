925804150af8e557d9e3c082d88db0ba35bfc94e
worashf <uppert83@gmail.com>
2022-09-07 19:45:51 +0300

    Add book specification

---
+++ spec/book_spec.rb
@@ -0,0 +1,13 @@
+require './classes/book'
+
+
+describe Book  do 
+    context "Initizlize book" do 
+        book = Book.new('o realy', 'Good', '12-06-2020', archived: false, id: 501)
+
+        it 'Should return publisher, cover state, published date, archived state and id' do
+          expect(book.can_be_archived?).to be false
+        end
+    end
+
+end
