ca779bff89577c68bdc1a19d0ee889538a8c7004
worashf <uppert83@gmail.com>
2022-09-08 20:58:14 +0300

    fix linters error

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,11 +1,15 @@
-require_relative '../classes/item'
+require './classes/item'
 
 describe Item do
-  item1 = Item.new('12-10-2020', false)
-  item2 = Item.new('05-12-2001', false)
+  context 'Test Item' do
+    publish_date = Time.now.year - 10
+    item1 = Item.new(publish_date, archived: false)
+    publish_date = Time.now.year - 11
+    item2 = Item.new(publish_date, archived: true)
 
-  it 'an_be_achived method works properly' do
-    expect item1.can_be_archived?.to eq(false)
-    expect item2.can_be_archived?.to eq(true)
+    it 'an_be_achived method works properly' do
+      expect(item1.move_to_archive).to be nil
+      expect(item2.move_to_archive).to be true
+    end
   end
 end
