49a2277d640448b623618e441cce39e2f0b7e82e
worashf <uppert83@gmail.com>
2022-09-09 14:05:40 +0300

    List additional values

--- helpers/display/list_book.rb
+++ helpers/display/list_book.rb
@@ -3,14 +3,14 @@ require_relative 'list'
 class ListBook < List
   def self.display(books)
     puts 'No book available' if books.empty?
+
     books.each do |book|
       book_status = book.archived ? 'Archived' : 'Not Archived'
       label = book.label.title
-      source = book.source.name
-      genre = book.genre.name
       author = book.author.first_name
-      puts "book id: #{book.id},  label: #{label},  source: #{source}, genre: #{genre}"
-      puts "author: #{author},  published date : #{book.published_date},  archvied status : #{book_status}"
+      source = book.source.name
+      puts "book id: #{book.id}, published date : #{book.published_date},  archvied status : #{book_status}"
+      puts "label: #{label}, author: #{author},soucrce: #{source} "
     end
   end
 end
--- helpers/display/list_music_album.rb
+++ helpers/display/list_music_album.rb
@@ -3,15 +3,13 @@ require_relative 'list'
 class ListMusicAlbum < List
   def self.display(music_albums)
     puts 'No book available' if music_albums.empty?
-    music_albums.each do |music_album|
-      music_album_status = music_album.archived ? 'Archived' : 'Not Archived'
-      label = music_album.label.title
-      source = music_album.source.name
-      genre = music_album.genre.name
-      author = music_album.author.first_name
-      puts "Music album id: #{music_album.id},label: #{label}, source: #{source}"
-      puts "Genre: #{genre},  author: #{author}"
-      puts "published date : #{music_album.published_date}, archvied status : #{music_album_status}"
+    music_albums.each do |m|
+      music_album_status = m.archived ? 'Archived' : 'Not Archived'
+      label = m.label.title
+      author = m.author.first_name
+      source = m.source.name
+      puts "Music id: #{m.id},published date : #{m.published_date}, archvied status : #{music_album_status}"
+      puts "label: #{label}, author: #{author},soucrce: #{source} "
     end
   end
 end
